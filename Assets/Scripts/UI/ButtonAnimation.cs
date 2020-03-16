6246ba6e415f21f561a346dbaa28663af870546a
DaanRoobrouck <54914795+DaanRoobrouck@users.noreply.github.com>
2020-03-16 21:01:08 +0100

    Start of Startscreen

---
+++ Assets/Scripts/UI/ButtonAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    
+    public void Pressed()
+    {
+        this.transform.parent.GetComponent<Animator>().SetBool("Pressed", true );
+    }
+
+    public void Selected()
+    {
+        this.transform.parent.GetComponent<Animator>().SetBool("Selected", true);
+    }
+
+    public void DeSelected()
+    {
+        this.transform.parent.GetComponent<Animator>().SetBool("Selected", false);
+
+    }
+}
