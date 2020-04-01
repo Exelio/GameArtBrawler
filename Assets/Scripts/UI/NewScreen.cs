dc0ba599abd101858257cc8b9208c9c1bc0233e4
DaanRoobrouck <54914795+DaanRoobrouck@users.noreply.github.com>
2020-04-01 12:58:08 +0200

    Current Update UI

--- Assets/Scripts/UI/NewScreen.cs
+++ Assets/Scripts/UI/NewScreen.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NewScreen : MonoBehaviour
 {
+
+    [SerializeField] private int _sceneIndex;
+
     //need the scene number of the game
     void StartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(_sceneIndex);
     }
 }
